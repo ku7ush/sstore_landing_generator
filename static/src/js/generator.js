f3454e40beccbd89c55db9365ff314f20309c6b8
ku7ush <ku7ush@gmail.com>
2023-05-29 10:30:37 +0200

    [Fix]

--- static/src/js/generator.js
+++ static/src/js/generator.js
@@ -42,29 +42,17 @@ odoo.define('sstore_landing_generator.website_province_form', function(require) 
                 $('input[name="province"]').on('change', function(){
                     var province = $('input[name="province"]').val();
                     var uppercaseprov = province.charAt(0).toUpperCase() + province.slice(1);
-                    var spaceKeyEvent = $.Event("keydown", { keyCode: 32 });
+                    var lowercaseprov = province.charAt(0).toLowerCase() + province.slice(1);
+                    //var spaceKeyEvent = $.Event("keydown", { keyCode: 32 });
                     if (province) {
-                        $('input[name="domain"]').val(province + '.scavolinistore.net');                
-                        $('input[name="domain"]').attr('value', province + '.scavolinistore.net');                
-                        $('input[name="name"]').val('Scavolini Store - ' + uppercaseprov);
-                        $('input[name="name"]').attr('value','Scavolini Store - ' + province);
+                        $('input[name="domain"]').val(province + '.s-store.net').change();
+                        $('input[name="name"]').val('Scavolini Store ' + uppercaseprov).change();
                         $('input[name="province"]').removeClass('uncompiled');
-                        //$('button[name="create_and_redirect_to_theme"]').trigger('click');
-
-                        $('input[name="name"]').focus();
-                        console.log('after 1st focus');
-                        $('input[name="name"]').trigger(spaceKeyEvent);
-                        console.log('should have space');
-                        $('input[name="domain"]').focus();
-                        console.log('after 2nd focus');
-                        $('input[name="domain"]').trigger(spaceKeyEvent);
-                        console.log('should have space');
                     }
                 })
             }
         },
         onWebsiteCreate: function(event) {
-            console.log('onWebsiteCreate');
         },
     });
 });
